354743ae6d2ea90b28d7f892af2866f2309818e3
Ben <bensongsyz@gmail.com>
2021-08-13 20:48:17 +0800

    Use mac address for Macintosh
            Use mac address to judge if connect to the ethernet in szu.
            rename ROUTER to GATEWAY

--- drcom/Linux/drcom.sh
+++ drcom/Linux/drcom.sh
@@ -24,24 +24,24 @@ APs_172="SZU_NewFi"
 
 # modify the following line with the mac address of the Ethernet router which needs to log in with "drcom.szu.edu.cn"
 # you can get the mac_address with "get_router_mac" function by uncomment get_router_mac at last
-ROUTER_MACs_192="xx:xx:xx:xx:xx:xx"
+GATEWAY_MACs_192="xx:xx:xx:xx:xx:xx"
 # sepreate with space
-#ROUTER_MAC_192="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
+#GATEWAY_MAC_192="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
 
 # SZU_Eth mask
 # modify the following line with the mac address of the Ethernet router which needs to log in with "172.30.255.2/a30.htm"
-ROUTER_MACs_172="xx:xx:xx:xx:xx:xx"
+GATEWAY_MACs_172="xx:xx:xx:xx:xx:xx"
 # sepreate with space
-#ROUTER_MAC_172="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
+#GATEWAY_MAC_172="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
 
 get_router_mac(){
     # sep with space
-    ROUTER_MACs_NOW=""
+    GATEWAY_MACs_NOW=""
     while read ETH_ADP; do
-	ROUTER_MACs_NOW="$ROUTER_MACs_NOW $(arp -i "$ETH_ADP" |awk 'NR>=2 {print $3}' | tr '\n' ' ')"
+	GATEWAY_MACs_NOW="$GATEWAY_MACs_NOW $(arp -i "$ETH_ADP" |awk 'NR>=2 {print $3}' | tr '\n' ' ')"
     done < <(echo "$ETH_ADPs" | tr ':' '\n')
-    ROUTER_MACs_NOW="$(echo $ROUTER_MACs_NOW|xargs)"
-    echo "$ROUTER_MACs_NOW"
+    GATEWAY_MACs_NOW="$(echo $GATEWAY_MACs_NOW|xargs)"
+    echo "$GATEWAY_MACs_NOW"
 }
 
 drcom_login(){
@@ -65,43 +65,42 @@ main(){
 	while read AP_ADP; do
 	    APs_NOW="$APs_NOW:$(iw dev "$AP_ADP" info 2> /dev/null |sed -n 's/\tssid\ \(.*\)/\1/p')"
 	done < <(echo "$AP_ADPs" | tr ':' '\n')
-	APs_NOW=$(echo $APs_NOW|sed 's/^:*//;s/:*$//')
+	APs_NOW="$(echo $APs_NOW|sed 's/^:*//;s/:*$//')"
 	#echo $APs_NOW
 
 	# sep with space
-	ROUTER_MACs_NOW=""
+	GATEWAY_MACs_NOW=""
 	while read ETH_ADP; do
-	    ROUTER_MACs_NOW="$ROUTER_MACs_NOW $(arp -i "$ETH_ADP" |awk 'NR>=2 {print $3}')"
+	    GATEWAY_MACs_NOW="$GATEWAY_MACs_NOW $(arp -i "$ETH_ADP" |awk 'NR>=2 {print $3}')"
 	done < <(echo "$ETH_ADPs" | tr ':' '\n')
-	ROUTER_MACs_NOW="$(echo $ROUTER_MACs_NOW|xargs)"
-	#echo $ROUTER_MACs_NOW
+	GATEWAY_MACs_NOW="$(echo $GATEWAY_MACs_NOW|xargs)"
+	#echo $GATEWAY_MACs_NOW
 
-
+	# login
 	while read AP_NOW; do
 	    #echo line="$AP_NOW";
 	    if [[ ":$APs_192:" =~ :"$AP_NOW": ]]; then # AP_NOW in $AP_192 ; ref: /etc/profile append_path()
 		# same as  [[ ":$APs_192:" =~ .*:"$AP_NOW":.* ]]
 		drcom_login "123456" "https://drcom.szu.edu.cn/a70.htm"
-		# drcom_login "123456" "http://drcom.szu.edu.cn/a70.htm"
-		continue
+		continue 2
 	    elif [[ ":$APs_172:" =~ :"$AP_NOW": ]]; then
 		drcom_login "%B5%C7%A1%A1%C2%BC" "http://172.30.255.2/a30.htm"
-		continue
+		continue 2
 	    fi
 	done < <(echo "$APs_NOW" | tr ':' '\n')
 
-	while read ROUTER_MAC_NOW; do
-	    #echo line="$AP_NOW";
-	    if [[ "$ROUTER_MACs_192" =~ "$ROUTER_MAC_NOW" ]]; then # AP_NOW in $AP_192 ; ref: /etc/profile append_path()
+	while read GATEWAY_MAC_NOW; do
+	    #echo line="$GATEWAY_MAC_NOW";
+	    if [[ "$GATEWAY_MACs_192" =~ "$GATEWAY_MAC_NOW" ]]; then # AP_NOW in $AP_192 ; ref: /etc/profile append_path()
 		drcom_login "123456" "https://drcom.szu.edu.cn/a70.htm"
 		#echo 192
-		continue
-	    elif [[ "$ROUTER_MACs_172" =~ "$ROUTER_MAC_NOW" ]]; then
+		continue 2
+	    elif [[ "$GATEWAY_MACs_172" =~ "$GATEWAY_MAC_NOW" ]]; then
 		drcom_login "%B5%C7%A1%A1%C2%BC" "http://172.30.255.2/a30.htm"
 		#echo 172
-		continue
+		continue 2
 	    fi
-	done < <(echo "$ROUTER_MACs_NOW") #MAC are seperated with space
+	done < <(echo "$GATEWAY_MACs_NOW") #MAC are seperated with space
     done
 }
 
--- drcom/Macintosh/drcom.sh
+++ drcom/Macintosh/drcom.sh
@@ -3,21 +3,22 @@
 CARDID='xxxxxx'
 PASSWORD='xxxxxx'
 
+# Wireless network card adapter
+AP_ADPs='wlp5s0'
 # sepreate with colon :
+#AP_ADPs='wlp5s0:wlan0'
 
-AP_ADPs='wlp5s0:wlan0'
+# Wired network card
+# sepreate with colon :
+ETH_ADPs='enp4s0:eth0'
 
 # get the names of all the adapters by:
 #networksetup -listallhardwareports
 # device name is like en0 en1 .etc
 # if the Hardware Port is not or are not only "Thunderbolt Ethernet", modify the following lines
-Eth_ADPs=''
-Eth_ADPs="$Eth_ADPs:$(networksetup -listallhardwareports |grep "Thunderbolt Ethernet" -A 1 |awk '{if (NR==2){print $2}}')"
-
-# Remove matching prefix pattern.
-Eth_ADPs=${Eth_ADPs%%:}
-# Remove matching suffix pattern.
-Eth_ADPs=${Eth_ADPs##:}
+ETH_ADPs='en5'
+#ETH_ADPs="$ETH_ADPs:$(networksetup -listallhardwareports |grep "Thunderbolt Ethernet" -A 1 |awk '{if (NR==2){print $2}}')"
+ETH_ADPs="$(echo "$ETH_ADPs"|sed 's/^:*//;s/:*$//')" # removing prefix suffix colon
 
 # connect to "drcom.szu.edu.cn"
 APs_192="SZU_WLAN"
@@ -29,9 +30,28 @@ APs_172="SZU_NewFi"
 # sepreate with colon :
 #APs_172="SZU_NewFi:xxxxx"
 
+# modify the following line with the mac address of the Ethernet router which needs to log in with "drcom.szu.edu.cn"
+# you can get the mac_address with "get_router_mac" function by uncomment get_router_mac at last
+GATEWAY_MACs_192="xx:xx:xx:xx:xx:xx"
+# sepreate with space
+#GATEWAY_MAC_192="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
+
 # SZU_Eth mask
-# Cannot distinguish which site should I log in by mask 
-Eth_MASKs="255.255.255.0"
+# modify the following line with the mac address of the Ethernet router which needs to log in with "172.30.255.2/a30.htm"
+GATEWAY_MACs_172="xx:xx:xx:xx:xx:xx"
+# sepreate with space
+#GATEWAY_MAC_172="xx:xx:xx:xx:xx:xx xx:xx:xx:xx:xx:xx"
+
+get_router_mac(){
+    # sep with space
+    GATEWAY_MACs_NOW=""
+    while read ETH_ADP; do
+	GATEWAY_IPs_NOW="$(ip -4 route list |grep "$ETH_ADP" | grep default |cut -d ' ' -f 3)"
+	GATEWAY_MACs_NOW="$GATEWAY_MACs_NOW $(ip neigh | grep "$GATEWAY_IPs_NOW" |awk '{print $5}')"
+    done < <(echo "$ETH_ADPs" | tr ':' '\n')
+    GATEWAY_MACs_NOW="$(echo $GATEWAY_MACs_NOW|xargs)" # removing the prefix and suffix space
+    echo "$GATEWAY_MACs_NOW"
+}
 
 drcom_login(){
     # Usage: drcom_login 0MKKey login_url
@@ -40,78 +60,61 @@ drcom_login(){
     # pinginfo='' # for test
     #empty when not connected
     if [ -z "$pinginfo" ]; then
-	if [ -n "$1" ];then
-	    # $1 $2 were assigned
-	    /usr/bin/curl -d "DDDDD=$CARDID" -d "upass=$PASSWORD" -d "0MKKey=$1" "$2" > /dev/null 2>& 1
-	    #echo a
-	else
-	    # default, used for ethernet connection
-	    # $1 $2 were empty, ping drcom.szu.edu.cn first if not connect to 172
-	    pinginfo=$(ping -c 1 -W 1 drcom.szu.edu.cn |grep " 0% packet loss")
-	    if [ -z "$pinginfo" ]; then
-		# drcom ping successful
-		#echo default drcom
-		/usr/bin/curl -d "DDDDD=$CARDID" -d "upass=$PASSWORD" -d "0MKKey=123456" "https://drcom.szu.edu.cn" > /dev/null 2>& 1
-	    else
-		/usr/bin/curl -d "DDDDD=$CARDID" -d "upass=$PASSWORD" -d "%B5%C7%A1%A1%C2%BC" "http://172.30.255.2/a30.htm" > /dev/null 2>& 1
-	    fi
-	fi
+	/usr/bin/curl -d "DDDDD=$CARDID" -d "upass=$PASSWORD" -d "0MKKey=$1" "$2" > /dev/null 2>& 1
     fi
 }
 
+main(){
+    while true
+    do
+	sleep 5
 
-while true
-do
-    sleep 5
+	AP_NOW=""
+	AP_NOW="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I |sed -n 's/ \{11\}SSID: \(.*\)/\1/p')"
+	#echo $AP_NOW
 
-    APs_NOW=""
-    #while read AP_ADP; do
-    #    APs_NOW="$APs_NOW:$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I |sed -n 's/ \{11\}SSID: \(.*\)/\1/p')"
-    #    #APs_NOW="$APs_NOW:$(iw dev $AP_ADP info 2> /dev/null |sed -n 's/\tssid\ \(.*\)/\1/p')"
-    #    #echo $APs_NOW
-    #done < <(echo "$AP_ADPs" | tr ':' '\n')
-    APs_NOW="$APs_NOW:$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I |sed -n 's/ \{11\}SSID: \(.*\)/\1/p')"
-    # Remove matching prefix pattern.
-    APs_NOW=${APs_NOW%%:}
-    # Remove matching suffix pattern.
-    APs_NOW=${APs_NOW##:}
-    # man bash ${parameter#word}
-    #echo $APs_NOW
+	#GATEWAY_IPs_NOW=""
+	#GATEWAY_MACs_NOW=""
+	## sep with return \n
+	#GATEWAY_IPs_NOW=$(ip -4 route list | grep default |cut -d ' ' -f 3) # find all the gateways, including the wireless
+	#GATEWAY_MACs_NOW=$(ip neigh | grep "$GATEWAY_IPs_NOW" |awk '{print $5}')
+	##echo $GATEWAY_MACs_NOW
+	
+	# sep with space
+	GATEWAY_MACs_NOW=""
+	while read ETH_ADP; do
+	    GATEWAY_IPs_NOW="$(ip -4 route list |grep "$ETH_ADP" | grep default |cut -d ' ' -f 3)"
+	    GATEWAY_MACs_NOW="$GATEWAY_MACs_NOW $(ip neigh | grep "$GATEWAY_IPs_NOW" |awk '{print $5}')"
+	done < <(echo "$ETH_ADPs" | tr ':' '\n')
+	GATEWAY_MACs_NOW="$(echo $GATEWAY_MACs_NOW|xargs)" # removing the prefix and suffix space
+	#echo "$GATEWAY_MACs_NOW"
 
-    Eth_MASKs_NOW=""
-    while read Eth_ADP; do
-	Eth_MASKs_NOW="$Eth_MASKs_NOW:$( ipconfig getoption $Eth_ADP subnet_mask )"
-    done < <(echo "$Eth_ADPs" | tr ':' '\n')
-    Eth_MASKs_NOW=${Eth_MASKs_NOW%%:}
-    Eth_MASKs_NOW=${Eth_MASKs_NOW##:}
-    #echo $Eth_MASKs_NOW
-
-
-    # The two ifs are classified according to the login url
-    # connect to "drcom.szu.edu.cn"
-    # https://stackoverflow.com/questions/27702452/loop-through-a-comma-separated-shell-variable
-
-    while read AP_NOW; do
-	#echo line="$AP_NOW";
+	# login
 	if [[ ":$APs_192:" =~ :"$AP_NOW": ]]; then # AP_NOW in $AP_192 ; ref: /etc/profile append_path()
 	    # same as  [[ ":$APs_192:" =~ .*:"$AP_NOW":.* ]]
-	    #echo 1
-	    drcom_login "123456" "http://192.168.255.235/a70.htm"
-	    # drcom_login "123456" "http://drcom.szu.edu.cn/a70.htm"
+	    echo 1
+	    drcom_login "123456" "https://drcom.szu.edu.cn/a70.htm"
 	    continue
 	elif [[ ":$APs_172:" =~ :"$AP_NOW": ]]; then
-	    #echo 2
+	    echo 2
 	    drcom_login "%B5%C7%A1%A1%C2%BC" "http://172.30.255.2/a30.htm"
 	    continue
 	fi
-    done < <(echo "$APs_NOW" | tr ':' '\n')
 
+	while read GATEWAY_MAC_NOW; do
+	    #echo line="$GATEWAY_MAC_NOW";
+	    if [[ "$GATEWAY_MACs_192" =~ "$GATEWAY_MAC_NOW" ]]; then # AP_NOW in $AP_192 ; ref: /etc/profile append_path()
+		drcom_login "123456" "https://drcom.szu.edu.cn/a70.htm"
+		#echo 192
+		continue 2
+	    elif [[ "$GATEWAY_MACs_172" =~ "$GATEWAY_MAC_NOW" ]]; then
+		drcom_login "%B5%C7%A1%A1%C2%BC" "http://172.30.255.2/a30.htm"
+		#echo 172
+		continue 2
+	    fi
+	done < <(echo "$GATEWAY_MACs_NOW"| tr ' ' '\n') #MAC are seperated with return
 
-    while read MASK_NOW; do
-	if [[ ":$MASK_NOW:" =~ :"$Eth_MASKs": ]]; then
-	    echo 0
-	    drcom_login
-	    continue
-	fi
-    done < <(echo "$Eth_MASKs_NOW" | tr ':' '\n')
-done
+    done
+}
+get_router_mac
+#main
